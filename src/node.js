9c0953a17c597c1a01f4d4fdc0583ea1a6d36842
Liu Chenling <Liuchenling@Ling.local>
2017-03-23 18:35:46 +0800

    feat: 支持搜索分词

--- src/node.js
+++ src/node.js
@@ -1,12 +1,59 @@
 'use strict';
 var path = require('path');
 var cwd = process.cwd();
+var nodejieba = require('nodejieba');
 
 module.exports = function(markdownData) {
   if (/\/theme\//i.test(markdownData.meta.filename)) {
     try {
       markdownData.package = require(path.join(cwd, markdownData.meta.filename, '../package.json'));
     } catch (err) {}
-  } 
+  }
+
+  if (/(en-US|_tmp\/(internal|external)Docs\/README)/.test(markdownData.meta.filename)) {
+    return markdownData;
+  }
+
+  const filename = markdownData.meta && markdownData.meta.filename
+      && markdownData.meta.filename.replace(/^_tmp\/(internal|external)Docs\/(zh-Hans|)\//, '')
+        .replace(/\.md$/i, '');
+  const title = markdownData.content && markdownData.content[1] &&
+  markdownData.content[1][0] === 'h1' && markdownData.content[1][1] || '';
+
+  const splitPath = filename.split('/');
+  let url = '';
+  switch (splitPath[0]) {
+    case 'guide':
+      url = `/guide/${splitPath[1]}`;
+      break;
+    case 'component':
+      url = `/component/${splitPath[1]}`;
+      break;
+    case 'theme':
+      if (filename === 'theme/template/Theme/how-to-contribute') {
+        url = '/theme/contribute';
+      } else {
+        url = `/theme/${splitPath[1]}`;
+      }
+      break;
+    default: break;
+  }
+  markdownData.url = url;
+  markdownData.path = filename;
+  markdownData.body = nodejieba.cut(getJsonmlString(markdownData.content), true).join(' ');
+  markdownData.title = title;
   return markdownData;
 };
+
+
+function getJsonmlString(jsonml) {
+  let result = '';
+  for (let i = 1; i < jsonml.length; i++) {
+    if (Array.isArray(jsonml[i])) {
+      result += getJsonmlString(jsonml[i]);
+    } else if (typeof jsonml[i] === 'string') {
+      result += jsonml[i] + ' ';
+    }
+  }
+  return result;
+}
